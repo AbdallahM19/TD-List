e60eb39dbe0bab51edfe06147cf81d1fb1b2ee5c
gehad <gehadgamalazzam@gmail.com>
2024-01-28 22:25:29 +0200

    add

--- To_do.py
+++ To_do.py
@@ -3,6 +3,7 @@
 add_task = __import__('TD-List').add_task
 remove_task = __import__('TD-List').remove_task
 print_tasks = __import__('TD-List').print_tasks
+
 task = input("Enter Task:")
 add_task(task)
 print_tasks(task)
