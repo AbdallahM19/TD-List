114318a84a5e0ca4a41bd189c7a3e47c4115b302
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-01-28 21:44:07 +0200

    td-list

---
+++ TD-List.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" To Do List App """
+
+tasks = []
+
+def add_task(task):
+    """ Add a task to the list. """
+    tasks.append(task)
+    print("Adding '{}' to the task list.".format(task))
+
+def remove_task(task):
+    """ Remove a specified task from the list. """
+    if task in tasks:
+        tasks.remove(task)
+        print("Removing '{}' from the task list.".format(task))
+    else:
+        print("Task ('{}') is not found in Tasks-List".format(task))
